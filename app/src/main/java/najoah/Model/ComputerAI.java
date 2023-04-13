3a25a9ad2f7297cd7ab3c0e3fef1e0333f51f7d6
Nathan Platcher <nateplatch@Nathans-MacBook-Pro.local>
2023-04-13 11:30:59 -0500

    ComputerAI

---
+++ app/src/main/java/najoah/Model/ComputerAI.java
@@ -0,0 +1,41 @@
+package najoah;
+
+import java.util.Random;
+
+//Class that will create a computer AI that will randomly select the next move
+
+public class ComputerAI
+{
+    private String nextMove;
+
+    public ComputerAI()
+    {
+        nextMove = "";
+    }
+
+    /** randomly selects the next move and returns it as a string. 
+    * Move selection is weighted, basicAttack-40%, block-40%, specialAttack-20%.
+    **/
+
+    public String getNextMove()
+    {
+        Random randomizer = new Random();
+
+        int num = randomizer.nextInt(100);
+        
+        if (num < 40)
+        {
+            nextMove = "basicAttack"; 
+        }
+        else if (num >= 40 && num <= 79)
+        {
+            nextMove = "block";
+        }
+        else if (num > 79)
+        {
+            nextMove = "specialAttack";
+        }
+
+        return nextMove;
+    }
+}
