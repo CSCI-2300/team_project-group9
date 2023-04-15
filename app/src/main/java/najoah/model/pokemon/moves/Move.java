49a3885542c3d361801a3d3f9d1ae96bec036c9d
nguzinski <nguzinski@slu.edu>
2023-04-15 14:04:32 -0500

    move changes

--- app/src/main/java/najoah/model/pokemon/moves/Move.java
+++ app/src/main/java/najoah/model/pokemon/moves/Move.java
@@ -2,17 +2,19 @@ package najoah.model.pokemon.moves;
 
 
 //setting this up as a super class to be used later for passing args
-public interface Move
+public class Move
 {
     protected String name;
 
     //move types can be "attack" or "block"
     public Move(String name)
-    { 
+    {
+        this.name = name; 
     }
 
     public String getName()
     {
+        return name;
     }
 
 }
