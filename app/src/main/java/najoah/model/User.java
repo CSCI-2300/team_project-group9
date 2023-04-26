ac1e882deb998ec56c00a482b57f476c86a7471c
Joe Marchi <joemarchi@Joes-MBP.lan>
2023-04-25 19:28:33 -0500

    edited the user class to store a vector of pokemon that will enable the user to switch to the next pokemon if one loses/dies. will need to call that in the makeMove method as: pokemons(0).get.makeMove(num) will leave it as is for now.

--- app/src/main/java/najoah/model/User.java
+++ app/src/main/java/najoah/model/User.java
@@ -1,17 +1,22 @@
 package najoah.model;
 
 import najoah.model.pokemon.Pokemon;
-import najoah.model.pokemon.moves.*;;
+import najoah.model.pokemon.moves.*;
+
+import java.util.Vector;
 
 //class will create a user that saves user wins and losses and pokemon history
 
 public class User {
     private Pokemon pokemon;
+
+    private Vector<Pokemon> pokemons;
     private int wins;
     private int losses;
 
     public User() {
         this.pokemon = new Pokemon();
+        this.pokemons = new Vector<>();
         this.wins = 0;
         this.losses = 0;
     }
@@ -45,5 +50,45 @@ public class User {
     public Pokemon getPokemon() {
         return pokemon;
     }
+
+    // Method here to add a pokemon to the vector
+    public void addPokemon(Pokemon pokemon)
+    {
+        pokemons.add(pokemon);
+    }
+
+    public Vector<Pokemon> getPokemons()
+    {
+        return pokemons;
+    }
+
+    //Method to switch pokemon if user loses current pokemon
+    public void switchPokemon()
+    {
+        String noMorePoke = "No more pokemon";
+        if (!pokemons.isEmpty())
+        {
+            pokemons.remove(0);
+
+            if (!pokemons.isEmpty())
+            {
+               pokemons.get(0);
+            }
+            else 
+            {
+               emptyPokemon(noMorePoke);
+            }
+        }
+        else
+        {
+            emptyPokemon(noMorePoke);
+        }
+    }
+
+    public String emptyPokemon(String noMorePoke)
+    {
+        return noMorePoke;
+    }
+
 }
 
